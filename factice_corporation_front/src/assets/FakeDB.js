ecc9244dd107ff8cc4936df2811199c9c7724fac
DrYruo <oury.jordan@hotmail.com>
2023-05-24 20:38:43 +0200

    ajout des fonctions de filtre

--- factice_corporation_front/src/assets/FakeDB.js
+++ factice_corporation_front/src/assets/FakeDB.js
@@ -15,10 +15,10 @@ export const clients = [
 ];
 
 export const rdvList = [
-  new RendezVous(1, employes[0], clients[0], new Date(), '09:00', '10:00'),
-  new RendezVous(2, employes[1], clients[1], new Date(), '10:00', '11:00'),
-  new RendezVous(3, employes[2], clients[2], new Date(), '11:00', '12:00'),
-  new RendezVous(4, employes[0], clients[1], new Date(), '14:00', '15:00'),
-  new RendezVous(5, employes[1], clients[2], new Date(), '15:00', '16:00'),
-  new RendezVous(6, employes[2], clients[0], new Date(), '16:00', '17:00')
+  new RendezVous(1, employes[0], clients[0], new Date(2023, 5, 14), '09:00', '10:00'),
+  new RendezVous(2, employes[1], clients[1], new Date(2023, 5, 14), '10:00', '11:00'),
+  new RendezVous(3, employes[2], clients[2], new Date(2023, 5, 21), '11:00', '12:00'),
+  new RendezVous(4, employes[0], clients[1], new Date(2023, 5, 26), '14:00', '15:00'),
+  new RendezVous(5, employes[1], clients[2], new Date(2023, 5, 18), '15:00', '16:00'),
+  new RendezVous(6, employes[2], clients[0], new Date(2023, 5, 30), '16:00', '17:00')
 ];
--- factice_corporation_front/src/components/ListeRendezVousComponent.vue
+++ factice_corporation_front/src/components/ListeRendezVousComponent.vue
@@ -1,22 +1,24 @@
 <template>
     <div class="container-fluid">
         <div class="btn-group mb-3 d-flex justify-content-between">
-            <button class="btn" :class="{ 'btn-dark': currentSort === 'date', 'btn-outline-dark': currentSort !== 'date' }" @click="sortRdv('date')">Trier par date</button>
-            <button class="btn" :class="{ 'btn-dark': currentSort === 'employe', 'btn-outline-dark': currentSort !== 'employe' }" @click="sortRdv('employe')">Trier par employé</button>
-            <button class="btn" :class="{ 'btn-dark': currentSort === 'client', 'btn-outline-dark': currentSort !== 'client' }" @click="sortRdv('client')">Trier par client</button>
-            <button class="btn btn-primary" @click="sortRdv('all')">Tous les RDV</button>
+            <button class="btn" :class="{ 'btn-dark': currentSort.includes('date'), 'btn-outline-dark': !currentSort.includes('date') }" @click="sortRdv('date')">Trier par date</button>
+            <button class="btn" :class="{ 'btn-dark': currentSort.includes('employe'), 'btn-outline-dark': !currentSort.includes('employe') }" @click="sortRdv('employe')">Trier par employé</button>
+            <button class="btn" :class="{ 'btn-dark': currentSort.includes('client'), 'btn-outline-dark': !currentSort.includes('client') }" @click="sortRdv('client')">Trier par client</button>
         </div>
 
         <div class="d-flex justify-content-between mb-3">
             <input class="form-control flex-grow-1 mx-2" type="date" v-model="selectedDate">
+            <button v-if="selectedDate" @click="selectedDate = null">X</button>
 
             <select class="form-control flex-grow-1 mx-2" v-model="selectedEmployer">
                 <option v-for="employe in employes" :key="employe.id" :value="employe">{{employe.nom}} {{employe.prenom}}</option>
             </select>
+            <button v-if="selectedEmployer" @click="selectedEmployer = null">X</button>
 
             <select class="form-control flex-grow-1 mx-2" v-model="selectedClient">
                 <option v-for="client in clients" :key="client.id" :value="client">{{client.nom}} {{client.prenom}}</option>
             </select>
+            <button v-if="selectedClient" @click="selectedClient = null">X</button>
         </div>
 
         <table class="table table-striped">
@@ -30,8 +32,8 @@
             </tr>
             </thead>
             <tbody>
-            <tr v-for="rdv in rdvList" :key="rdv.id">
-                <td>{{rdv.date}}</td>
+            <tr v-for="rdv in filteredRdvList" :key="rdv.id">
+                <td>{{ formatDate(rdv.date)}}</td>
                 <td>{{rdv.heureDebut}}</td>
                 <td>{{rdv.heureFin}}</td>
                 <td>{{rdv.employe.nom}} {{rdv.employe.prenom}}</td>
@@ -54,49 +56,95 @@ export default {
       selectedDate: '',
       selectedEmployer: null,
       selectedClient: null,
-      currentSort: '',
+      currentSort: [],
+      localRdvList: []
     }
   },
-
 
   created() {
     store.commit('setEmployes', employes);
     store.commit('setClients', clients);
     store.commit('setRdvList', rdvList);
+    this.localRdvList = [...this.rdvList];
   },
 
-  computed: mapState({
-    rdvList: state => state.rdvList,
-    employes: state => state.employes,
-    clients: state => state.clients,
-  }),
+  computed: {
+    ... mapState ({
+      rdvList: state => state.rdvList,
+      employes: state => state.employes,
+      clients: state => state.clients,
+    }),
+
+    filteredRdvList() {
+      return this.localRdvList.filter (rdv => {
+        let isMatch = true;
+
+        // Si une date est sélectionnée, on vérifie si elle correspond à celle du rendez-vous
+        if (this.selectedDate) {
+          const selectedDate = new Date(this.selectedDate);
+          const rdvDate = new Date(rdv.date);
+
+          // Comparer les dates en ignorant l'heure
+          isMatch = isMatch && selectedDate.toISOString().substring(0, 10) === rdvDate.toISOString().substring(0, 10);
+        }
+
+        // Si un employé est sélectionné, vérifiez si c'est celui du rendez-vous
+        if (this.selectedEmployer) {
+          isMatch = isMatch && rdv.employe.id === this.selectedEmployer.id;
+        }
+
+        // Si un client est sélectionné, vérifiez si c'est celui du rendez-vous
+        if (this.selectedClient) {
+          isMatch = isMatch && rdv.client.id === this.selectedClient.id;
+        }
+
+        return isMatch;
+      });
+    },
+  },
 
   methods: {
-      sortRdv(sortBy) {
-          // Si le bouton actuellement sélectionné est cliqué à nouveau, annulez le tri.
-          if (this.currentSort === sortBy) {
-              this.currentSort = '';
-          } else {
-              this.currentSort = sortBy;
+    sortRdv(sortBy) {
+      // Vérifiez si le critère de tri est déjà actif
+      const index = this.currentSort.indexOf(sortBy);
+
+      if (index !== -1) {
+        // Si le critère de tri est déjà actif, le supprimer pour désactiver le tri
+        this.currentSort.splice(index, 1);
+      } else {
+        // Si le critère de tri n'est pas actif, l'ajouter à la liste des tris actifs
+        this.currentSort.push(sortBy);
+      }
+
+      // Effectuez le tri approprié en fonction de currentSort.
+      this.localRdvList.sort((a, b) => {
+        for (let sortBy of this.currentSort) {
+          let comparison = 0;
+          switch(sortBy) {
+            case 'date':
+              comparison = new Date(a.date) - new Date(b.date);
+              break;
+            case 'employe':
+              comparison = a.employe.nom.localeCompare(b.employe.nom);
+              break;
+            case 'client':
+              comparison = a.client.nom.localeCompare(b.client.nom);
+              break;
           }
 
-          // Effectuez le tri approprié en fonction de currentSort.
-          switch(this.currentSort) {
-              case 'date':
-                  this.rdvList.sort((a, b) => a.date - b.date);
-                  break;
-              case 'employe':
-                  this.rdvList.sort((a, b) => a.employe.nom.localeCompare(b.employe.nom));
-                  break;
-              case 'client':
-                  this.rdvList.sort((a, b) => a.client.nom.localeCompare(b.client.nom));
-                  break;
-              default:
-                  // Dans le cas où aucun tri n'est sélectionné ou pour "all", remettre les rdv dans leur état original.
-                  this.rdvList = [...rdvList];
-                  break;
+          // Si cette clé de tri différencie les deux éléments, retournez la comparaison
+          if (comparison !== 0) {
+            return comparison;
           }
-      },
+        }
+        // Si aucune clé de tri ne différencie les deux éléments, ils sont considérés comme égaux
+        return 0;
+      });
+    },
+
+    formatDate(date) {
+      return new Date(date).toLocaleDateString('fr-FR');
+    },
   }
 }
 </script>
